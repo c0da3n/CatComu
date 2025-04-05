d190232aa9e12759f6f6229a3d61a07664c21f5e
c0da3n <sky@codawn.dev>
2025-04-05 19:39:44 -0400

    Initialize

---
+++ CatComu_FE/src/app/layout.tsx
@@ -0,0 +1,16 @@
+import StyledComponentsRegistry from "./_lib/registry";
+import "./_styles/globals.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <body>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
+    </html>
+  );
+}
